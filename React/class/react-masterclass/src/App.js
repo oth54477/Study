fdf059cc46e4795fd1da2c2c6b1d6a7879f1fd24
oth54477 <oth5447@naver.com>
2023-01-18 01:22:53 +0900

    study

--- React/class/react-masterclass/src/App.js
+++ React/class/react-masterclass/src/App.js
@@ -1,35 +1,23 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor}
   }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotationAnimation} 1s linear infinite;
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Title>hello</Title>
     </Wrapper>
   );
 }
