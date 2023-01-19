62d7488d6f86b10cba71ec2e8c3a6b7f68502711
oth54477 <oth5447@naver.com>
2023-01-19 09:30:06 +0900

    test

--- React/class/react-masterclass/src/index.js
+++ React/class/react-masterclass/src/index.js
@@ -16,7 +16,7 @@ const lightTheme = {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
