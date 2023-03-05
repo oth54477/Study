27ffaa9f7506fd443cd5ef49cdd0caf46eabcf12
oth54477 <oth5447@naver.com>
2023-03-05 23:04:13 +0900

    Study Spring

--- Spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -21,11 +23,12 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원X
-        validateDuplicateMember(member);
 
-        memberRepository.save(member);
-        return member.getId();
+            // 같은 이름이 있는 중복 회원X
+            validateDuplicateMember(member);
+
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -39,7 +42,8 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+            return memberRepository.findAll();
+
     }
 
     public Optional<Member> findOne(Long memberId) {
